ced0cdb71607de7589e5ef8aa5915ba2dea84140
Michael Nickey <mnickey@gmail.com>
2016-12-21 17:57:36 -0800

    disemvowel trolls

--- disemvowel_trolls.py
+++ disemvowel_trolls.py
@@ -1,4 +1,4 @@
 def disemvowel(string):
     vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
-    string = ''.join([l for l in string if l not in vowels]);
+    string = ''.join([l for l in string if l not in vowels])
     return string
