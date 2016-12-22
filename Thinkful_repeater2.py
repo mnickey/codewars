d35208c7b10ce14f32311bcd6300bff6c0a17043
Michael Nickey <mnickey@gmail.com>
2016-12-21 17:43:10 -0800

    Thinkful repeater2

--- Thinkful_repeater2.py
+++ Thinkful_repeater2.py
@@ -10,3 +10,4 @@ assert_equals(repeater('yo', 3), '"yo" repeated 3 times is: "yoyoyo"')
 assert_equals(repeater('WuB', 6), '"WuB" repeated 6 times is: "WuBWuBWuBWuBWuBWuB"')
 assert_equals(repeater('code, eat, code, sleep... ', 2),
               '"code, eat, code, sleep... " repeated 2 times is: "code, eat, code, sleep... code, eat, code, sleep... "')
+
