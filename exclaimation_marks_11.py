e804eec47bf384a5d6f01d203003d498e3c0e651
Michael Nickey <mnickey@gmail.com>
2016-12-21 18:10:33 -0800

    count positives sum negatives

---
+++ exclaimation_marks_11.py
@@ -0,0 +1,9 @@
+def replace_exclamation(s):
+    answer = ""
+    vowels = ['a','e','i','o','u']
+    for item in s:
+        if item.lower() in vowels:
+            answer += '!'
+        else:
+            answer += item
+    return answer
